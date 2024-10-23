7dbd09ef29bd7b52d91729ba7cb3a2ed62cddbd3
Elena Andreeva <elena.and.kam@gmail.com>
2024-10-23 21:15:52 +0300

    feat: add decision

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -592,8 +592,8 @@ function group(/* array, keySelector, valueSelector */) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], (x) => x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.map(childrenSelector).flat();
 }
 
 /**
@@ -608,9 +608,19 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  let findEl;
+  if (indexes.length === 1) {
+    findEl = arr[indexes[0]];
+  } else if (indexes.length === 2) {
+    findEl = arr[indexes[0]][indexes[1]];
+  } else if (indexes.length === 3) {
+    findEl = arr[indexes[0]][indexes[1]][indexes[2]];
+  }
+  return findEl;
 }
+
+// ПЕРЕДЕЛАТЬ ЧЕРЕХ REDUCE
 
 /**
  * Swaps the head and tail of the specified array:
